9345e23119c05205f6e986051b4170892a4dbf0a
Vincent PORTA-SCARTA <vincent.porta@outlook.fr>
2017-01-30 15:01:15 -0500

    Add P1 "decouverte"

---
+++ decouverte/pages/1/script.js
@@ -0,0 +1,14 @@
+// To check the answer
+var stringAnswer;
+
+function initAnswer() {
+   stringAnswer = "";
+}
+
+function checkAnswer() {
+    if(stringAnswer === "OK") {
+        enable_next();
+    }
+}
+
+//------------------------------------------------//
