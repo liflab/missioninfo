ddb769fc0f13f6f10acf2ddd5d5099b64abd637e
Vincent PORTA-SCARTA <vincent.porta@outlook.fr>
2017-01-30 16:50:44 -0500

    Add P4 "decouverte"

---
+++ decouverte/pages/4/script.js
@@ -0,0 +1,20 @@
+// To check the answer
+var stringAnswer;
+var blockAfficheDown;
+
+function initAnswer() {
+   stringAnswer = "";
+   blockAfficheDown = false;
+}
+
+function checkAnswer() {
+    if (blockAfficheDown && !(stringAnswer === "")) {
+        bootbox.alert({
+        message: '<div class="text-center">Voici le texte que tu as affiché<br>'+ stringAnswer +'</div>',
+        backdrop: true,
+        callback: enable_next()
+        });
+    }
+}
+
+//------------------------------------------------//
