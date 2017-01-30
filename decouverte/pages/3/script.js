f41e0562d767bff6e4827af0b0f9056816313562
Vincent PORTA-SCARTA <vincent.porta@outlook.fr>
2017-01-30 16:50:24 -0500

    Add P3 "decouverte"

---
+++ decouverte/pages/3/script.js
@@ -0,0 +1,14 @@
+// To check the answer
+var stringAnswer;
+
+function initAnswer() {
+   stringAnswer = "";
+}
+
+function checkAnswer() {
+    if(stringAnswer === "CHIEN") {
+        enable_next();
+    }
+}
+
+//------------------------------------------------//
