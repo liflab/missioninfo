7bee420d9b7dad96c05c07d45fc1ca8f1c7449e7
Vincent PORTA-SCARTA <vincent.porta@outlook.fr>
2017-04-08 16:09:13 -0400

    Update "codage" P4

--- codage/pages/4/script.js
+++ codage/pages/4/script.js
@@ -1,13 +1,29 @@
 //
 
-displayInfo();
+popupInfo("Les ordinateurs ne connaissent pas les lettres! Ils utilisent des nombres \n pour coder les lettres comme les agents secrets. Entraine-toi à ecrire \n des messages codés en retrouvant les lettres qui correspondent \n à chaque nombre. Ecrit chaque lettre dans la case associée.");
 
 function verifiyString() {
 
-    var str_value = document.getElementById("ASCII_text").value;
+    var isValid = true;
 
-    if(str_value === "BONJOUR A TOI") {
+    for (var i = 1; i <= 12; i++) {
+        var code_ASCII = parseInt(document.getElementById("ASCII_" + i).innerHTML);
+        var letter = document.getElementById("letter_" + i).value.toUpperCase();
+
+        if (letter === "") {
+            letter = " ";
+        }
+
+        if (!(String.fromCharCode(code_ASCII) === letter)) {
+            isValid = false;
+            break;
+        }
+    }
+
+    if (isValid) {
         popupGood();
+        document.getElementById("btn_run_prog").style.display = "none";
+        document.getElementById("btn_next_exercise").style.display = "block";
     }
     else {
         popupNotGood();
@@ -15,6 +31,8 @@ function verifiyString() {
 }
 
 function reinit_text() {
-    document.getElementById("ASCII_text").value = "";
+    for (var i = 1; i <= 12; i++) {
+        document.getElementById("letter_" + i).value = "";   
+    }
 }
 //------------------------------------------------//
