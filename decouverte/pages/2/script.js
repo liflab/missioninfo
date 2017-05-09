a96676100e7144f0a9c46d937e57b85c377df721
Vincent PORTA-SCARTA <vincent.porta@outlook.fr>
2017-05-09 17:41:17 -0400

    Correct bug "decouverte" part 2

--- decouverte/pages/2/script.js
+++ decouverte/pages/2/script.js
@@ -8,7 +8,8 @@ function initAnswer() {
 }
 
 function checkAnswer() {
-    if(stringAnswer === "OK") {
+    // Trick to verify if blocks are attach together
+    if (stringAnswer === "OK" && Blockly.getMainWorkspace().getAllBlocks()[0].childBlocks_[0] !== undefined) {
         enable_next();
     }
     else {
